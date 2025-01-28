6a24ce518147a48364694d40fa53ab29babe6a37
aleqsandreapkhazava <miaafxazava@gmail.com>
2025-01-28 21:31:37 +0400

    second commit

--- Day 70/cw/cw.py
+++ Day 70/cw/cw.py
@@ -1,1 +1,21 @@
-print("no classwork")
+# name = ("alex")
+# name2 = name.capitalize()
+# print(name2)
+
+
+
+# def capitalize_first_letter(s):
+#     return s.capitalize()
+
+
+
+
+
+
+# a = "I am learning Python and Python is awesome."
+# u = index_of_(a)
+# print(u)  # Output: 17
+
+sentence = "I am learning Python and Python is awesome."
+position = find_python_position(sentence)
+print(position)  # Output: 17
---
+++ Day 73/cw/cw.js
@@ -0,0 +1,34 @@
+
+
+// 1))))
+// let a = {
+//     name: "მარიამი",
+//     age: 30,
+//     gender: "ქალი",
+//     height: "12cm",
+//     phonenubmere: "1234567890"
+// };
+
+// console.log(a.height);
+
+// 2))))))
+    // let numbersa = {
+    //     luwi: [1,3,5,7,9],
+    //     kenti: [2,4,6,8,10],
+    // }
+
+    // for (let i = 0; i < numbersa['luwi'].length; i++) {
+    //     console.log(numbersa['luwi'][i], numbersa['kenti'][i]);
+    // }
+
+
+
+    // 3))))))
+
+function obj(object){
+
+}
+
+obj("alex")
+obj("14")
+????????????????????
